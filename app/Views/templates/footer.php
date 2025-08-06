dd068660e53dfca0a41ad5d48b94b81ead752dc0
niklos01 <s4niklos@uni-trier.de>
2025-08-06 16:11:00 +0200

    refactor(footer)

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,4 +1,9 @@
-<footer>footer</footer>
+<footer class="bg-light text-center py-3 mt-5 border-top">
+    <div class="container">
+        <small>&copy; <?= date('Y') ?> Fortgeschrittene Webentwicklung – Uni-Projekt</small>
+    </div>
+</footer>
+
 
 <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
