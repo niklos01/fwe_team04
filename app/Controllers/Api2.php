da5186b93792c763546065fea75b4910bc1b75f5
niklos01 <s4niklos@uni-trier.de>
2025-08-07 13:30:55 +0200

    feat: implemented crud api route

---
+++ app/Controllers/Api2.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\PersonModel;
+use CodeIgniter\RESTful\ResourceController;
+
+class Api extends ResourceController
+{
+
+    protected $format = 'json';
+
+    public function index()
+    {
+        $model    = new PersonModel();
+        $personen = $model->getPersonen();
+        return $this->respond($personen);
+    }
+
+    public function reqWithAuth()
+    {
+        // Get the Authorization header
+        $authHeader = $this->request->getHeaderLine('Authorization');
+
+        // Check if Authorization header exists and starts with "Bearer "
+        if (empty($authHeader) || ! str_starts_with($authHeader, 'Bearer ')) {
+            return $this->response->setStatusCode(401)->setJSON(["error" => 'Sie sind nicht autorisiert.']);
+        }
+
+        // Extract the token
+        $token = trim(substr($authHeader, 7));
+
+        // Validate the token (replace this with your actual token validation logic)
+        if ($token !== "Team#04") {
+            return $this->response->setStatusCode(403)->setJSON(["error" => 'Ungültiger Bearer-Token.']);
+
+        }
+
+        // Alles ok , dann return data
+        $model = new PersonModel();
+
+        $personen = $model->getPersonen();
+        return $this->respond($personen);
+
+    }
+
+}
